abb06a99f0c645a0a9a1da118c3ed26ba5f7f320
Viliam Lejcik <lejcik@gmail.com>
2025-03-15 13:51:08 +0100

    reverted nanosvg from vcpkg.json, as it has to be patched

--- src/svg.cpp
+++ src/svg.cpp
@@ -6,9 +6,9 @@
 #include "svg.h"
 
 #define NANOSVG_IMPLEMENTATION
-#include <nanosvg/nanosvg.h>
+#include "nanosvg/nanosvg.h"
 #define NANOSVGRAST_IMPLEMENTATION
-#include <nanosvg/nanosvgrast.h>
+#include "nanosvg/nanosvgrast.h"
 
 CSVGSprite SVGArrowRight;
 CSVGSprite SVGArrowRightSmall;
